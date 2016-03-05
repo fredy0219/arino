7703c8e36817213585b3460b5c4d15eba1fd517f
max30012540 <max30012540@hotmail.com>
2016-03-05 23:39:45 +0700

    fix bug boot
    fix bug server not autorun on boot

--- server/install.sh
+++ server/install.sh
@@ -8,6 +8,6 @@ echo "" > /usr/arino/hexfile.hex
 chmod 777 /usr/arino/hexfile.hex
 wget http://file.host-1gb.com/arino/arino -O /etc/init.d/arino
 chmod 755 /etc/init.d/arino
-ln -s /etc/init.d/arino /etc/rc.d/S99arino
+ln -s "../init.d/arino" "/etc/rc.d/S99arino"
 /etc/init.d/arino start
 echo "Complete installation"
